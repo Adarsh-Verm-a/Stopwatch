c2d6ab4d412e1bfce86f9b1149dcc0c8e6f003d5
Adarsh-Verm-a <vadarsh717@gmail.com>
2024-03-11 19:51:38 +0530

    everything ready

--- Timer/timer.js
+++
@@ -1,6 +0,0 @@
-function toggleTheme() {
-    const body = document.body;
-    body.classList.toggle('dark-theme');
-    const stopwatch = document.getElementById('stopwatch');
-    stopwatch.classList.toggle('dark-theme');
-}
